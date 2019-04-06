f84d128abbe1299212d0af3734e7f08ab9c1daf5
Denis Chuzhov <densmorgon@gmail.com>
2019-04-07 00:36:53 +0300

    Hotfix.

--- logger.py
+++ logger.py
@@ -7,21 +7,17 @@ logger = logging.getLogger()
 
 class Logger(object):
     @staticmethod
-    @boundaries(logger.info)
     def log_success(message: str) -> None:
         logger.info(crayons.green(message))
 
     @staticmethod
-    @boundaries(logger.info)
     def log_warning(message: str) -> None:
         logger.info(crayons.magenta(message))
 
     @staticmethod
-    @boundaries(logger.error)
     def log_error(message: str) -> None:
         logger.error(crayons.red(message))
 
     @staticmethod
-    @boundaries(logger.info)
     def log_system(message: str) -> None:
         logger.info(crayons.yellow(message))
